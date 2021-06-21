099b6d6402979eeb0f6d754743eb3835501a86fb
apostolos <apostolos.tapsas@vectorcamp.gr>
2021-06-21 17:27:46 +0300

    rshift changes

--- src/Maths/rshift.cpp
+++ src/Maths/rshift.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 
+/*here i put some new code*/
 uint8_t Rshift(void * ptr, unsigned int n){
     uint8_t buf[16] = {0};
+    for (int i=0; i<16; i++){buf[i] = buf[i]<<0;}
     return 0; 
 }
